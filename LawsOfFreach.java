f3d06c99e58464f94ff5b0c35b9a837907688e79
Vexarian <julianpasker@gmail.com>
2024-01-23 12:47:47 +0100

    Finished exercise 19: The Laws of Freach

---
+++ LawsOfFreach.java
@@ -0,0 +1,20 @@
+public class LawsOfFreach
+{
+	public static void main(String[] args)
+	{
+		int[] array = { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+		int currentSmallest = Integer.MAX_VALUE; // Start higher than anything in the array.
+		for (int i : array)
+		{
+			if (i < currentSmallest)
+				currentSmallest = i;
+		}
+		System.out.println(currentSmallest);
+
+		int total = 0;
+		for (int i : array)
+			total += i;
+		double average = (double)total / array.length;
+		System.out.println(average);
+	}
+}
