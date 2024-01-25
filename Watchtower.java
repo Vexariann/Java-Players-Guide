d6464d06cec7815762b6564df3956d23c2b25e11
Vexarian <julianpasker@gmail.com>
2024-01-25 12:09:28 +0100

    Finished exercise 20 part 2: Taking a Number
    Added the askForNumber method to Watchtower.java

--- Watchtower.java
+++ Watchtower.java
@@ -6,16 +6,19 @@ public class Watchtower
 	{
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Hello sarge, i've heard you received coordinates of the closest enemy camp?");
-		System.out.println("\nWhat's the X coordinate?");
-		String xcoordinate = input.next();
-		System.out.println("\nWhat's the Y coordinate?");
-		String ycoordinate = input.next();
+		// System.out.println("Hello sarge, i've heard you received coordinates of the closest enemy camp?");
+		// System.out.println("\nWhat's the X coordinate?");
+		// String xcoordinate = input.next();
+		// System.out.println("\nWhat's the Y coordinate?");
+		// String ycoordinate = input.next();
+		
+		int xcoordinateInt = askForNumber("\nWhat's the X coordinate?");
+		int ycoordinateInt = askForNumber("\nWhat's the Y coordinate?");
 		
 		try
 		{
-			int xcoordinateInt = Integer.parseInt(xcoordinate);
-			int ycoordinateInt = Integer.parseInt(ycoordinate);
+			// int xcoordinateInt = Integer.parseInt(xcoordinate);
+			// int ycoordinateInt = Integer.parseInt(ycoordinate);
 			
 			String DirectionX = "";
 			String DirectionY = "";
@@ -61,6 +64,24 @@ public class Watchtower
 		{
 			System.out.println("\nERROR: one of your inputs was not a valid number. Please try again.\n\n");
 			main(args);					
+		}
+	}
+	
+	public static int askForNumber(String text)
+	{
+		Scanner input = new Scanner(System.in);
+		int numberInt = 0;
+		
+		System.out.println(text);
+		String number = input.next();
+		try
+		{
+			return numberInt = Integer.parseInt(number);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("\nERROR: your input was not a valid number. Please try again.\n\n");
+			return askForNumber(text);
 		}
 	}
 }
