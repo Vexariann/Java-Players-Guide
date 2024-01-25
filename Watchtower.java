e5d531aab4381d35aa514cd51309bee7e7363305
Vexarian <julianpasker@gmail.com>
2024-01-25 12:10:47 +0100

    Removed redundant try catch block

--- Watchtower.java
+++ Watchtower.java
@@ -6,65 +6,47 @@ public class Watchtower
 	{
 		Scanner input = new Scanner(System.in);
 		
-		// System.out.println("Hello sarge, i've heard you received coordinates of the closest enemy camp?");
-		// System.out.println("\nWhat's the X coordinate?");
-		// String xcoordinate = input.next();
-		// System.out.println("\nWhat's the Y coordinate?");
-		// String ycoordinate = input.next();
-		
 		int xcoordinateInt = askForNumber("\nWhat's the X coordinate?");
 		int ycoordinateInt = askForNumber("\nWhat's the Y coordinate?");
+			
+		String DirectionX = "";
+		String DirectionY = "";
 		
-		try
+		if (ycoordinateInt == 0 && xcoordinateInt == 0)
 		{
-			// int xcoordinateInt = Integer.parseInt(xcoordinate);
-			// int ycoordinateInt = Integer.parseInt(ycoordinate);
-			
-			String DirectionX = "";
-			String DirectionY = "";
-			
-			if (ycoordinateInt == 0 && xcoordinateInt == 0)
-			{
-				System.out.println("\nThat's the location of our watchtower you dummy!");
-				return;
-			}
-			
-			// x coordinate
-			if(xcoordinateInt > 0)
-			{
-				DirectionX = "east";
-			}
-			else if (xcoordinateInt < 0)
-			{
-				DirectionX = "west";
-			}
-			else
-			{
-				DirectionX = "";
-			}
-			
-			// y coordinate
-			if(ycoordinateInt > 0)
-			{
-				DirectionY = "north";
-			}
-			else if (ycoordinateInt < 0)
-			{
-				DirectionY = "south";
-			}
-			else
-			{
-				DirectionY = "";
-			}
-			
-			System.out.println("The enemy appears to be coming from " + DirectionY + DirectionX);
-			
+			System.out.println("\nThat's the location of our watchtower you dummy!");
+			return;
 		}
-		catch(NumberFormatException e)
+		
+		// x coordinate
+		if(xcoordinateInt > 0)
 		{
-			System.out.println("\nERROR: one of your inputs was not a valid number. Please try again.\n\n");
-			main(args);					
+			DirectionX = "east";
 		}
+		else if (xcoordinateInt < 0)
+		{
+			DirectionX = "west";
+		}
+		else
+		{
+			DirectionX = "";
+		}
+		
+		// y coordinate
+		if(ycoordinateInt > 0)
+		{
+			DirectionY = "north";
+		}
+		else if (ycoordinateInt < 0)
+		{
+			DirectionY = "south";
+		}
+		else
+		{
+			DirectionY = "";
+		}
+		
+		System.out.println("The enemy appears to be coming from " + DirectionY + DirectionX);
 	}
 	
 	public static int askForNumber(String text)
