a5282588eb61e2dbbde19cc1277ab063ec6d8151
Vexarian <julianpasker@gmail.com>
2024-01-25 12:03:48 +0100

    Finished exercise 20 part 1: Taking a Number

---
+++ TakingANumber.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class TakingANumber
+{
+	public static void main(String[] args)
+	{
+		int number1 = askForNumber("Fill in a number to display on screen");
+		System.out.println(number1);
+		
+		int number2 = askForNumberInRange("Fill in a number between 1 and 10 to display on screen", 1, 10);
+		System.out.println(number2);
+	}
+	
+	public static int askForNumber(String text)
+	{
+		Scanner input = new Scanner(System.in);
+		int numberInt = 0;
+		
+		System.out.println(text);
+		String number = input.next();
+		try
+		{
+			return numberInt = Integer.parseInt(number);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("\nERROR: your input was not a valid number. Please try again.\n\n");
+			return askForNumber(text);
+		}
+	}
+	
+	public static int askForNumberInRange(String text, int min, int max)
+	{
+		Scanner input = new Scanner(System.in);
+		int numberInt = 0;
+		
+		System.out.println(text);
+		String number = input.next();
+		try
+		{
+			numberInt = Integer.parseInt(number);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("\nERROR: your input was not a valid number. Please try again.\n\n");
+			return askForNumberInRange(text, min, max);
+		}
+		
+		if (numberInt > min && numberInt < max)
+		{
+			return numberInt;	
+		}
+		else
+		{
+			System.out.println("\nERROR: your input is out of range. Please try again.\n\n");
+			return askForNumberInRange(text, min, max);
+		}
+	}
+}
