cd7c63694ee5ac89039fdff1984c5de197205d66
Vexarian <julianpasker@gmail.com>
2024-03-21 15:24:01 +0100

    Removed comment

--- puzzle15/game/GameLogic.java
+++ puzzle15/game/GameLogic.java
@@ -2,29 +2,6 @@ package puzzle15.game;
 
 import java.util.*;
 
-// 15 puzzle program flow:
-
-// randomly scramble numbers from 1 to 15 in a 4x4 board.
-
-// the scramble can be done with an arraylist, this is probably the easiest way, so I have to look
-// into arraylist and how to scramble that. iirc you cannot scramble in a regular array
-
-// empty tile will ALWAYS be on the top left.
-// This can be done with 2d array/arraylist
-// in the console the player will have to choose a direction where they want to rearrange the numbers.
-
-// The program will then check which number is in that slot and put it in the previous slot.
-// The empty space will move and that will be the new starting point
-
-// after every move, check if the game array is equal to the answer array
-
-// also add 1 to the move counter to track the amount of moves a player has made
-
-// bonus: highscore
-
-// 	IMPORTANT!!!
-// zie het als een rubix cube: begin de game in complete state en randomize de zetten in het begin
-
 public class GameLogic{
 		
 	ArrayList<ArrayList<Integer>> board = new ArrayList<>();
@@ -42,7 +19,7 @@ public class GameLogic{
 	Random rand = new Random();
 	Scanner scanner = new Scanner(System.in);
 	
-	//this bool is here to prevent the console from being spammed with error messages while the board is automatically shuffling the numbers.
+	//this bool is here to prevent the console from being spammed with error messages while the board is automatically shuffling.
 	boolean isAutoGenerating = false;
 		
 	public void generateGame(){		
